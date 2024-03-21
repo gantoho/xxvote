9a9a000fdad5a39d073bae356931ecf3eab09ca0
Ganto <i@ganto.icu>
2024-03-21 20:56:30 +0800

    feat: add tools/ecode.go

--- app/logic/login.go
+++ app/logic/login.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"xxvote/app/model"
+	"xxvote/app/tools"
 )
 
 type User struct {
@@ -19,18 +20,20 @@ func LoginPost(context *gin.Context) {
 	var user User
 	err := context.ShouldBind(&user)
 	if err != nil {
-		context.JSON(http.StatusOK, map[string]string{
-			"msg": "参数错误",
+		context.JSON(http.StatusOK, tools.Ecode{
+			Message: err.Error(), // 这里有风险
 		})
 	}
 	ret := model.GetUser(user.Name)
 	if ret.Id < 1 || ret.Password != user.Password {
-		context.JSON(http.StatusOK, map[string]string{
-			"msg": "用户名或密码错误",
+		context.JSON(http.StatusOK, tools.Ecode{
+			Message: "账号或密码错误",
 		})
 		return
 	}
 	context.SetCookie("name", user.Name, 3600, "/", "localhost", false, true)
-	//context.JSON(http.StatusOK, ret)
-	context.Redirect(http.StatusMovedPermanently, "index")
+	context.JSON(http.StatusOK, tools.Ecode{
+		Message: "登录成功",
+	})
+	//context.Redirect(http.StatusMovedPermanently, "index")
 }

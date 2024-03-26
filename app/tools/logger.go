b93e955b452f0f9efd9f51243be9520e954dbd55
Ganto <i@ganto.icu>
2024-03-26 21:54:52 +0800

    feat: use logrus

--- app/tools/logger.go
+++ app/tools/logger.go
@@ -6,15 +6,21 @@ import (
 	"os"
 )
 
-var Logger *logrus.Logger
+var Logger *logrus.Entry
 
 func NewLogger() {
-	Logger = logrus.New()
-	Logger.SetLevel(logrus.DebugLevel)
+	logStore := logrus.New()
+	logStore.SetLevel(logrus.DebugLevel)
 
 	// 同时写到多个输出
 	w1 := os.Stdout
 	w2, _ := os.OpenFile("./vote.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	Logger.SetOutput(io.MultiWriter(w1, w2))
-	Logger.SetFormatter(&logrus.JSONFormatter{})
+	logStore.SetOutput(io.MultiWriter(w1, w2))
+	logStore.SetFormatter(&logrus.JSONFormatter{})
+
+	Logger = logStore.WithFields(logrus.Fields{
+		"name": "ganto",
+		"app":  "voteV2",
+	})
+	//logStore.AddHook() // 出现非常严重的问题，直接邮箱或者微信报警，日志分割，塞入一些自定义的字段
 }
